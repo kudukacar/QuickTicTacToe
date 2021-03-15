6b5bf6f6fec5a01c06e8c2f3ff3d31a56665cbf7
Manju <manju.thomas083@gmail.com>
2021-03-15 16:16:00 -0400

    display outcome

--- lib/presenter.rb
+++ lib/presenter.rb
@@ -1,12 +1,13 @@
 class Presenter
   def present(board)
-    <<~BOARD
+    formatted_board = <<~BOARD
        #{get_cell(board, 1)} | #{get_cell(board, 2)} | #{get_cell(board, 3)} 
       ---+---+---
        #{get_cell(board, 4)} | #{get_cell(board, 5)} | #{get_cell(board, 6)} 
       ---+---+---
        #{get_cell(board, 7)} | #{get_cell(board, 8)} | #{get_cell(board, 9)} 
       BOARD
+      return formatted_board + "\n#{board.outcome}"
   end
 
   def get_cell(board, position)
